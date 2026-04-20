7b9df5760dea18d6cd100f763c84f06a8a379eac
Vishnu Nair <vnair91@gmail.com>
2026-04-20 17:02:03 +0530

    Fix SFTTrainer: tokenizer -> processing_class for TRL 1.2

--- src/iter3/01_train_sft.py
+++ src/iter3/01_train_sft.py
@@ -80,7 +80,7 @@ def main():
 
     trainer = SFTTrainer(
         model=model,
-        tokenizer=tokenizer,
+        processing_class=tokenizer,
         train_dataset=ds,
         args=training_args,
     )
